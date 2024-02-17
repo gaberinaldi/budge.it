2324a06fef76544266443ab187ce382f81cc5217
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2024-02-16 20:07:03 -0700

    added-js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn').addEventListener('click', function(event) {
+    event.preventDefault();
+  
+    var income = document.getElementById('income').value;
+    var rent = document.getElementById('Rent').value;
+    var car = document.getElementById('Car').value;
+  
+    console.log('Income:', income);
+    console.log('Rent/Mortgage:', rent);
+    console.log('Car Payment & Insurance:', car);
+  });

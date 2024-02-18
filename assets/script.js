4a44e2a8d269084a81af335453b1a83d4c86b387
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2024-02-17 22:10:38 -0700

    added-inputs

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,19 @@ document.getElementById('btn').addEventListener('click', function(event) {
     event.preventDefault();
   
     const income = document.getElementById('income').value;
-    const rent = document.getElementById('Rent').value;
-    const car = document.getElementById('Car').value;
-    const total = income - rent - car;
-  
-    console.log('Income:', income);
-    console.log('Rent/Mortgage:', rent);
-    console.log('Car Payment & Insurance:', car);
-    console.log(total);
+    const rent = document.getElementById('rent').value;
+    const car = document.getElementById('car').value;
+    const utilities = document.getElementById('utilities').value;
+    const groceries = document.getElementById('groceries').value;
+    const gas = document.getElementById('gas').value;
+    const additional = document.getElementById('additional').value;
+    const total = income - rent - car - utilities - groceries - gas - additional;
 
     const savingsTotal = document.getElementById('savingsTotal');
-    savingsTotal.innerHTML = 'You saved a total of '  + total +'!';
+
+    if (total < 0) {
+      savingsTotal.innerHTML = 'Uh oh, you lost a total of ' + total + ' this month. You should consider cutting back on some expenses.';
+    } else if (total > 0) {
+        savingsTotal.innerHTML = 'Great job, you saved a total of '  + total +' this month!';
+    }
   });

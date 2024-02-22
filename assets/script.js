ff02a33bd62bd5389ddbfa71cea5d1eef577edec
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2024-02-21 18:22:40 -0700

    cards-js

--- assets/script.js
+++ assets/script.js
@@ -17,4 +17,28 @@ document.getElementById('btn').addEventListener('click', function(event) {
     } else if (total > 0) {
         savingsTotal.innerHTML = 'Great job, you saved a total of '  + total +' this month!';
     }
+
+    const rentIncome = (rent / income) * 100;
+    const rentPercentage = document.getElementById('rentPercentage');
+    rentPercentage.innerHTML = rentIncome.toFixed(2) + '%';
+
+    const carIncome = (car / income) * 100;
+    const carPercentage = document.getElementById('carPercentage');
+    carPercentage.innerHTML = carIncome.toFixed(2) + '%'; 
+
+    const utilitiesIncome = (utilities / income) * 100;
+    const utilitiesPercentage = document.getElementById('utilitiesPercentage');
+    utilitiesPercentage.innerHTML = utilitiesIncome.toFixed(2) + '%';
+
+    const groceriesIncome = (groceries / income) * 100;
+    const groceriesPercentage = document.getElementById('groceriesPercentage');
+    groceriesPercentage.innerHTML = groceriesIncome.toFixed(2) + '%';
+
+    const gasIncome = (gas / income) * 100;
+    const gasPercentage = document.getElementById('gasPercentage');
+    gasPercentage.innerHTML = gasIncome.toFixed(2) + '%';
+
+    const additionalIncome = (additional / income) * 100;
+    const additionalPercentage = document.getElementById('additionalPercentage');
+    additionalPercentage.innerHTML = additionalIncome.toFixed(2) + '%';
   });
